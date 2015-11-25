9b9c9a3632655b610b69168098eea44549252c7b
MDouriez <marie.douriez@gmail.com>
2015-11-25 01:08:03 -0800

    small changes in main, new file utils with useful functions, performs mvt recognition

--- main.py
+++ main.py
@@ -2,12 +2,11 @@
 import numpy as np
 import processFile
 import matplotlib.pyplot as plt
+import utils
 
-fileIn = "door1211.csv"
+fileIn = "doorOC.csv"
 fileOut = "doorOut2.csv"
-
-### if new file, need to process data
-# processFile.processFile(fileIn, fileOut)
+# fileOut = "doorOCOut.csv"
 
 ## extract data from csv file
 data = np.genfromtxt(fileOut, delimiter=',')
@@ -19,90 +18,24 @@ accY = acc[:,1]
 accZ = acc[:,2]
 m,n = acc.shape
 
-DaccX = np.diff(accX)
-DaccY = np.diff(accY)
-DaccZ = np.diff(accZ)
-Dtime = np.diff(time)
-print Dtime
-
-## derivative
-DX = DaccX / Dtime
-DY = DaccY / Dtime
-DZ = DaccZ / Dtime
-
-## removes mean
-meanX = np.mean(accY[:50])
+## removes mean with first values (up to index)
+index = 50
+meanX = np.mean(accY[:index])
 accX -= meanX
-meanY = np.mean(accY[:50])
+meanY = np.mean(accY[:index])
 accY -= meanY
-meanZ = np.mean(accZ[:50])
+meanZ = np.mean(accZ[:index])
 accZ -= meanZ
 
-## velocity
-VXi = np.zeros(m-1)
-VYi = np.zeros(m-1)
-VZi = np.zeros(m-1)
-Y = np.zeros(m-1)
-Z = np.zeros(m-1)
+###
+VX, VY, VZ = utils.computeVelocity (accX, accY, accZ, time)
+DX, DY, DZ = utils.computeDerivative (accX, accY, accZ, time)
 
-for i in range(m-1):
-    # VXi[i] = ((accX[i]+accX[i+1])/2) * Dtime[i]
-    # VZi[i] = ((accZ[i]+accZ[i+1])/2) * Dtime[i]
-    # VYi[i] = ((accY[i]+accY[i+1])/2) * Dtime[i]
-    VXi[i] = accX[i] * Dtime[i]
-    VZi[i] = accZ[i] * Dtime[i]
-    VYi[i] = accY[i] * Dtime[i]
-VX = np.cumsum(VXi)
-VY = np.cumsum(VYi)
-VZ = np.cumsum(VZi)
 
-#compute values that will be used for thresholds
-DVZ = np.diff(VZ)
-maxDVZ = np.max(np.absolute(DVZ[:50]))
-DVY = np.diff(VY)
-maxDVY = np.max(np.absolute(DVY[:50]))
-
-#set thresholds
-maxDerivativeY = np.max(np.absolute(DY[:60]))
-maxDerivativeZ = np.max(np.absolute(DZ[:60]))
-thresholdMinY = np.min(accY[:30])
-thresholdMaxY = np.max(accY[:30])
-thresholdMinZ = np.min(accZ[:30])
-thresholdMaxZ = np.max(accZ[:30])
-
-# define booleans
-booleanY = accY>thresholdMaxY
-booleanZ1 = accZ>thresholdMaxZ
-booleanZ2 = accZ<thresholdMinZ
-booleanZ3 = np.absolute(DZ)>maxDerivativeZ
-booleanZ4 = np.absolute(DVZ)>maxDVZ
-booleanY4 = np.absolute(DVY)>maxDVY
-mvtZ1 = np.where(booleanZ1,1,0)[:m-2]
-mvtZ2 = np.where(booleanZ2,1,0)[:m-2]
-mvtZ3 = np.where(booleanZ3,1,0)[:m-2]
-mvtZ4 = np.where(booleanZ4,1,0)[:m-2]
-mvtY4 = np.where(booleanY4,1,0)[:m-2]
-mvtY1 = np.where(booleanY,1,0)[:m-2]
-mvtY2 = np.where(np.absolute(DY)>maxDerivativeY,1,0)[:m-2]
-
-mvt = mvtY1 | mvtY2 |mvtZ1 | mvtZ2 | mvtZ3 | mvtZ4| mvtY4
-i = 0
-while i<m-2:  #loop to handles gaps in data aquisition
-    if mvt[i]==0:
-        print i
-        t0 = time[i]
-        j = i+1
-        while j<m-2 :
-            if (mvt[j]==0):
-                j +=1
-            else: break
-        print "j: ", j
-        timeDiff = time[j] - t0
-        if timeDiff < 1:
-            mvt[i:j] = 1
-        i = j+1
-    else:
-        i +=1
+p = 10
+timeInterval0 = 0.7
+timeInterval1 = 0.5
+mvt = utils.recognizeMvt (accX, accY, accZ, time, p, timeInterval0, timeInterval1)
 
 data = np.zeros((m-1,2))
 data[:,0] = time[:m-1]
@@ -113,6 +46,10 @@ data[:,0] = time[:m-1]
 # plt.plot(time, accX, color='g')
 plt.plot(time, accY, color='b')
 plt.plot(time, accZ, color='r')
+#
+# plt.plot(time0, acc0X, color='g')
+# plt.plot(time0, acc0Y, color='b')
+# plt.plot(time0, acc0Z, color='r')
 
 ## plot velocity
 # plt.plot(time[:m-1], VX, color='g')
