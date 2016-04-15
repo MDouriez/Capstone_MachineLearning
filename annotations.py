e5ce3e000b223b3397a820acf5a569935d71a2c8
MDouriez <marie.douriez@gmail.com>
2016-04-14 19:13:21 -0700

    update

--- annotations.py
+++ annotations.py
@@ -20,13 +20,13 @@ def end_format(row):
         return datetime.datetime.combine(row['Day'],row['End'])
 
 
-fileIn = 'Annotations Capstone.xlsx'
+fileIn = 'Annotations Capstone 2.xlsx'
 
 
 ### Preprocessing Bathroom
 shower_time = 5 # time (in minutes to shower)
 
-df_bath = pd.read_excel(fileIn,'Bathroom',3,parse_cols='A:D')   # reading the excel file
+df_bath = pd.read_excel(fileIn,'Bathroom', 3, parse_cols='A:D')   # reading the excel file
 df_bath.head()
 
 df_bath['Day'] = df_bath['Day'].fillna(method='ffill')  # filling NaT day values with the current day
@@ -55,7 +55,7 @@ df_bed.loc[df_bed.End.notnull(),'End'] = df_bed[df_bed.End.notnull()].apply(end_
 df_bed.loc[df_bed.Activity == 'C','End'] = df_bed.loc[df_bed.Activity == 'C','Start'] + datetime.timedelta(minutes=2)
 df_bed['End'] = pd.to_datetime(df_bed['End'])         # converting to Y-M-D HH:MM:SS
 df_bed = df_bed[['Start','End','Activity']]           # dropping the day
-df_bed['Activity'] = df_bed['Activity'].replace(['C','B'],['Change','Sleep'])
+df_bed['Activity'] = df_bed['Activity'].replace(['C','B'],['Other','Sleep'])
 
 df_bed['Pres_bed'] = pd.Series(1., index=df_bed.index)
 
@@ -92,10 +92,24 @@ df_kitch.head()
 
 ### Final dataframe
 
-start_date = '25/02/2016 00:01:00'
-days=14
-periods = days*24*60 # numer of periods: 2 weeks with a frequency of 1 min
-rng = pd.date_range(start_date, periods=periods, freq='min')
+#start_date = '20/02/2016 00:01:00'
+#end_date = '2016-03-18 20:57:00'
+
+start_date = min (min(df_kitch['Start']), min(df_bed['Start']), min(df_bath['Start'])) - datetime.timedelta(minutes=5)
+
+max_bed = max(df_bed['End'])
+max_kitch = max(df_kitch['End'])
+max_bath = max(df_bath['End'])
+end_date = max([max_bed, max_kitch, max_bath]) + datetime.timedelta(minutes=5)
+
+print "start:", start_date, "end:", end_date
+
+
+#days=14
+#periods = days*24*60 # number of periods: 2 weeks with a frequency of 1 min
+#rng = pd.date_range(start = start_date, periods=periods, freq='min')
+
+rng = pd.date_range(start = start_date, end = end_date, freq='min')
 df = pd.Series('Other', index=rng, name='Activity')    # default activity is 'Other'
 df = pd.DataFrame(df)
 
@@ -108,21 +122,41 @@ df.head()
 
 ## filling in the values in df
 
+
+
 # Bathroom activities
 for i in range(0,len(df_bath.index)):
     start = df_bath.loc[i,'Start']
     end = df_bath.loc[i,'End']
     time_index = (df.index >= start) & (df.index <= end)
     df.loc[time_index,'Activity'] = df_bath.loc[i,'Activity']
-    df.loc[time_index,'Pres_bath'] = 1
+
+    # adding noise
+    bf_S = 1
+    af_S = 2
+    bf_T = 1
+    af_T = 1
+    if df_bath.loc[i, 'Activity'] == 'Shower':
+        time_index_noise = (df.index >= start - datetime.timedelta(minutes=bf_S)) & (df.index <= end + datetime.timedelta(minutes=af_S))
+    if df_bath.loc[i, 'Activity'] == 'Toilet':
+        time_index_noise = (df.index >= start - datetime.timedelta(minutes=bf_T)) & (df.index <= end + datetime.timedelta(minutes=af_T))
+    df.loc[time_index_noise,'Pres_bath'] = 1
 
 # Bedroom activities
 for i in range(0,len(df_bed.index)):
-    start = df_bed.loc[i,'Start']
-    end = df_bed.loc[i,'End']
+    start = df_bed.loc[i, 'Start']
+    end = df_bed.loc[i, 'End']
     time_index = (df.index >= start) & (df.index <= end)
-    df.loc[time_index,'Activity'] = df_bed.loc[i,'Activity']
-    df.loc[time_index,'Pres_bed'] = 1
+    df.loc[time_index, 'Activity'] = df_bed.loc[i,'Activity']
+
+    # adding noise
+    bf_B = 5
+    af_B = 1
+    if df_bed.loc[i,'Activity'] == 'Sleep':
+        time_index_noise = (df.index >= start - datetime.timedelta(minutes=bf_B)) & (df.index <= end + datetime.timedelta(minutes=af_B))
+        df.loc[time_index_noise,'Pres_bed'] = 1
+    else:
+        df.loc[time_index,'Pres_bed'] = 1
 
 # Kitchen activities
 for i in range(0,len(df_kitch.index)):
@@ -130,13 +164,25 @@ for i in range(0,len(df_kitch.index)):
     end = df_kitch.loc[i,'End']
     time_index = (df.index >= start) & (df.index <= end)
     df.loc[time_index,'Activity'] = df_kitch.loc[i,'Activity']
-    df.loc[time_index,'Pres_kitch'] = 1
+
+    bf_cook = 2
+    af_cook = 2
+    if df_kitch.loc[i,'Activity'] == 'Cook':
+        time_index_noise = (df.index >= start - datetime.timedelta(minutes=bf_cook)) & (df.index <= end + datetime.timedelta(minutes=af_cook))
+        df.loc[time_index_noise,'Pres_kitch'] = 1
+    if df_kitch.loc[i,'Activity'] == 'Eat':
+        #print df.index[time_index]
+        df.loc[time_index,'Pres_bed'] = 1 # eating in bedroom
 
 
 y = df['Activity']
 presence = df[['Pres_bath', 'Pres_kitch', 'Pres_bed']]
-presence.to_csv("presence.csv")
-y.to_csv('y.csv')
+
+# export to csv files
+presence.to_csv("presence_alldata.csv")
+y.to_csv('y_alldata.csv')
+
+print "Success"
 
 
 
